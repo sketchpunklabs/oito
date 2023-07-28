feeaee5a2a4801585b583a681541279e05db1b7e
Pedro Sousa <sketchpunk@gmail.com>
2023-07-28 11:10:38 -0400

    DualQuat

--- packages/oop/src/index.ts
+++ packages/oop/src/index.ts
@@ -3,6 +3,7 @@ import Vec2         from './Vec2';
 import Vec3         from './Vec3';
 import Mat4         from './Mat4';
 import Transform    from './Transform';
+import DualQuat     from './DualQuat';
 import Vec3Wizzy    from './Vec3Wizzy'
 
-export { Quat, Vec2, Vec3, Mat4, Transform, Vec3Wizzy };
+export { Quat, Vec2, Vec3, Mat4, Transform, DualQuat, Vec3Wizzy };
