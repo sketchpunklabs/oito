a1a486fb0fb134d96ec21544456f2f53fb612777
Pedro Sousa <sketchpunk@gmail.com>
2023-07-28 11:22:52 -0400

    cleanup

--- packages/ray/src/intersects/intersectObb.ts
+++ packages/ray/src/intersects/intersectObb.ts
@@ -1,5 +1,5 @@
-import { Vec3 }         from '@oito/oop';
-import Ray              from '../Ray';
+import { Vec3 } from '@oito/oop';
+import Ray      from '../Ray';
 
 export class RayObbResult{
     posEntry    = new Vec3();
@@ -14,9 +14,9 @@ export class RayObbResult{
 
 export function intersectObb( ray: Ray, center:ConstVec3, xDir: ConstVec3, yDir: ConstVec3, zDir: ConstVec3, halfLen: ConstVec3, result ?: RayObbResult ): boolean{
     const rayDelta	= new Vec3( center ).sub( ray.posStart ); // Distance between Ray start and Box Position
-    let tMin 		= 0;
-    let tMax 		= 1000000;
-    let minAxis		= 0;    // Which axis hit, X:0, Y:1, Z:2
+    let tMin        = 0;
+    let tMax        = 1000000;
+    let minAxis     = 0;    // Which axis hit, X:0, Y:1, Z:2
     let maxAxis     = 0;
     let axis        : ConstVec3;
     let nomLen      : number;
@@ -28,16 +28,16 @@ export function intersectObb( ray: Ray, center:ConstVec3, xDir: ConstVec3, yDir:
 
     for( let i=0; i < 3; i++){
         axis        = list[ i ];
-        nomLen		= Vec3.dot( axis, rayDelta ); 	    // Get the length of Axis and distance to ray position
-        denomLen	= Vec3.dot( ray.vecLength, axis );  // Get Length of ray and axis
+        nomLen      = Vec3.dot( axis, rayDelta ); 	    // Get the length of Axis and distance to ray position
+        denomLen    = Vec3.dot( ray.vecLength, axis );  // Get Length of ray and axis
 
-        if( Math.abs( denomLen ) > 0.00001 ){	// Can't divide by Zero
+        if( Math.abs( denomLen ) > 0.00001 ){ // Can't divide by Zero
             min = ( nomLen - halfLen[i] ) / denomLen;
             max = ( nomLen + halfLen[i] ) / denomLen;
 
             if( min > max ){  tmp = min; min = max; max = tmp; }    // Swap
-            if( min > tMin ){ tMin = min; minAxis = i; }			// Biggest Min
-            if( max < tMax ){ tMax = max; maxAxis = i; }			// Smallest Max
+            if( min > tMin ){ tMin = min; minAxis = i; }            // Biggest Min
+            if( max < tMax ){ tMax = max; maxAxis = i; }            // Smallest Max
 
             if( tMax < tMin ) return false;
         }else if(
