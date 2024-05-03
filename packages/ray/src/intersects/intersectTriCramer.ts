8feb27a255aac8cee04fc5f4711ac34be823d8e0
Pedro Sousa <sketchpunk@gmail.com>
2024-05-03 11:59:16 -0400

    update to triangle-ray intersect

--- packages/ray/src/intersects/intersectTriCramer.ts
+++ packages/ray/src/intersects/intersectTriCramer.ts
@@ -3,7 +3,8 @@ import Ray      from '../Ray';
 
 const EPS = 0.000001;
 
-function det3x3( a: ConstVec3, b: ConstVec3, c: ConstVec3 ): number {
+// All det's function
+function det3x3_old( a: ConstVec3, b: ConstVec3, c: ConstVec3 ): number {
     const a00 = a[0], a01 = a[1], a02 = a[2];
     const a10 = b[0], a11 = b[1], a12 = b[2];
     const a20 = c[0], a21 = c[1], a22 = c[2];
@@ -14,6 +15,21 @@ function det3x3( a: ConstVec3, b: ConstVec3, c: ConstVec3 ): number {
         a02 * (  a21 * a10 - a11 * a20 )
     );
 }
+
+function det3x3_new( a: ConstVec3, b: ConstVec3, c: ConstVec3 ): number {
+    return Vec3.dot( a, new Vec3().fromCross( b, c ) );
+}
+
+function det3x3( a: ConstVec3, b: ConstVec3, c: ConstVec3 ): number {
+    // cross( b, c ) 
+    const x = b[1] * c[2] - b[2] * c[1];
+    const y = b[2] * c[0] - b[0] * c[2];
+    const z = b[0] * c[1] - b[1] * c[0];
+    
+    // dot( a, [xyz] )
+    return a[0] * x + a[1] * y + a[2] * z;
+}
+
 
 // Solve with Cramer rule
 // https://x.com/lisyarus/status/1786327676120117683
@@ -34,7 +50,7 @@ export default function intersectTriCramer( ray: Ray, v0: ConstVec3, v1: ConstVe
     }
 
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    // Solve 3xe Equation
+    // Solve 3x3 Equation
     const det = det3x3( m0,  m1,  m2 );
     if( Math.abs( det ) < EPS ) return false;
 
@@ -46,6 +62,7 @@ export default function intersectTriCramer( ray: Ray, v0: ConstVec3, v1: ConstVe
     const u   = d1 / det;
     const v   = d2 / det;
 
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     // Results
     const hit = ( t >= 0 && u >= 0 && v >= 0 && ( u + v ) <= 1 );
     if( hit && out ) ray.directionAt( t, out );
